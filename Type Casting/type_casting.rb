136985a0d4aa2789e7f81677e7a34586c2a48964
Nagendran M <nagendransri3@gmail.com>
2021-07-27 14:06:55 +0530

    initiate commit

---
+++ Type Casting/type_casting.rb
@@ -0,0 +1,20 @@
+#Convert into Integer
+input_1 = gets.chomp.to_i
+
+#Convert into Float
+input_2 = gets.chomp.to_f
+
+#Convert into String
+input_3 = gets.chomp.to_s
+
+#Convert into Fraction
+input_4 = gets.chomp.to_r
+
+#Convert into Imaginary & Real
+input_5 = gets.chomp.to_c
+
+puts input_1
+puts input_2
+puts input_3
+puts input_4
+puts input_5
