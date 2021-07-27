5eeaa6144b0ce49226f36d1ff18d214e52b4b264
Nagendran M <nagendransri3@gmail.com>
2021-07-27 14:12:59 +0530

    initiate commit

---
+++ Conditional Statements/all_cond-statements.rb
@@ -0,0 +1,13 @@
+# if, elsif, else
+
+input_number = gets.chomp.to_i
+
+if input_number==1
+  puts "One"
+elsif input_number==2
+  puts "Two"
+elsif input_number==3
+  puts "Three"
+else
+  puts "Else"
+end
---
+++ Conditional Statements/switch_case.rb
@@ -0,0 +1,13 @@
+#case, when
+
+input_number = gets.chomp.to_i
+
+
+case input_number
+when 1
+  puts "One"
+when 2
+  puts "Two"
+else
+  puts "Else"
+end
